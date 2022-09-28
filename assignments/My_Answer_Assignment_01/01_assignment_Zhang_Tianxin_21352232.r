6c9d3b2be9952996dcdff8d9f4441ecf7e2b6ef8
CaesarCoder <zhangt4@tcd.ie>
2022-09-28 13:18:59 +0100

    Caesar's answer

---
+++ assignments/My_Answer_Assignment_01/01_assignment_Zhang_Tianxin_21352232.r
@@ -0,0 +1,133 @@
+options(jupyter.rich_display = FALSE)
+
+
+# E1
+l <- list(
+    mapply(assign, letters, 1:26),
+    A = array(1:60, c(5,2,3)),
+    M = matrix(1:50, c(10,5))
+)
+
+l
+
+# Exercise 1:
+
+# Your code goes here
+
+## All consonants from the first character vector
+LETTERS <- l[[1]]
+str(LETTERS)
+CONSONANTS <- LETTERS[c("b", "c", "d", "f", "g", "h", "j", "k",
+                        "l", "m", "n", "p", "q", "r", "s", 
+                        "t", "v", "w", "x", "y", "z")]
+CONSONANTS
+
+str(CONSONANTS) ## 21 character vectors
+
+## Second columns in all dimensions from the array A
+SDA <- l$A[, 2, ]
+
+SDA
+
+
+#  Odd rows from the matrix M
+
+MC1 <- l$M[, 1]
+
+MROWS_ODD <- 1$M[ ,]
+
+
+
+
+# E2
+
+set.seed(2022)
+vec <- sample(c("1", "0", "o"), size = 100, replace = TRUE)
+
+# Exercise 2:
+
+# Your code goes here
+vec
+typeof(vec)   # it is character vector 
+vec_nu<- as.numeric(vec)
+str(vec)
+CO <- which(vec == "o")  # "o"
+str(CO) # there are 40 "o" in total, 40%
+ifelse()
+vec_NEW <- ifelse(vec == "o",0, vec)
+vec_NEW
+
+
+
+
+
+## Answer: its type is character vector
+# what proportion of the vector elements did we fail to convert correctly? 40%
+
+
+
+
+
+
+ 
+books <- c(
+  "Dubliners",
+  "Ulysses",
+  "War and Peace",
+  "Normal People",
+  "Crime and Punishment",
+  "The Picture of Dorian Gray"
+)
+
+# Exercise 3:
+
+# Your code goes here
+
+NCB <- nchar(books)
+NCB # 9 7 13 13 20 26
+
+mean(NCB)
+
+PERC <- * 100
+
+
+
+# E4
+set.seed(2022)
+educ <- rpois(1000, 2)
+educ <- ifelse(educ > 4, 4, educ)
+
+# Exercise 4:
+
+# Your code goes here
+
+educ_FC <- as.factor(educ)
+educ_FC
+tab_edu <- table(educ_FC) 
+tab_edu # 0, 140; 1, 247; 2, 267; 3, 181; 4, 138. Secondary 
+# education coded as 2 is the most frequent 
+edu_REORDER <- factor(tab_edu, levels = c("Secondary Education", 
+                                         "No formal education", 
+                                         "Primary Education",
+                                         "College", 
+                                         "Post-graduate Education"))
+edu_REORDER
+
+
+# E5
+char_1 <- unlist(strsplit("kitten", split = ""))
+char_2 <- unlist(strsplit("sitting", split = ""))
+
+char_1
+
+char_2
+
+# Exercise 5:
+
+# Your code goes here
+char_1_fac <- as.factor(char_1)
+char_2_fac <- as.factor(char_2)
+num_tra <- 0
+num_tra  <- ifelse(char_2_fac == char_1_fac, char_2_fac, num_tra +1)
+
+
